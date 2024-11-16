1bb23e0ca78ebe15e51280db99b59f4800ab0a0a
shimelis <shimelisget2@gmail.com>
2024-11-16 08:51:01 -0800

    font icons and other features added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,9 @@
 
-import Logo from "../../assets/logo.jpg"
+import Logo from "../../assets/logo.jpg";
+import { FaSearch } from "react-icons/fa";
+
+
+
 export default function Navbar ()
 {
   return (
@@ -8,24 +12,46 @@ export default function Navbar ()
         {/* upper navbar*/}
         <div>
             <div>
-                < div className="h-50 w-80 bg-slate-300" >
-                    <a href="#">
+                < div className="self-center flex bg-blue-200 gap-2 content-center pe-0 space-x-2">
+                    <a href="#" className="w-20 h-30">
                         <img src={Logo} alt="logo"  />
                     </a>
-                   
-                </div>
-               
+                    <div className="ms-0 flex">
+                    <input type="text" placeholder="search" />
+                    <span><FaSearch /></span>
+                    <span>Search</span><span><img/></span>
+                     </div>
+                     <div className="m">
+                        <a href="#">Sign Up</a>
+                     </div>
+                     <div className="space-x-1">
+                        <a href="#">Register</a>
+                     </div>
+                </div> 
+                
             </div>
         </div>
-        <h1> We are in the community for community</h1>
-
     </div>
     <div>
         {/* lower navbar*/}
+        <div className="bg-amber-400">
+            <ul className="flex justify-between gap-px bg-amber-600">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Credit</a></li>
+                <li><a href="#">saving</a></li>
+                <li><a href="#">Investment</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Training</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About us</a></li>
+            </ul>
+              
+        </div>
+
     </div>
     
   </div>
   );
-  
 }
 
